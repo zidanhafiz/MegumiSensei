a7a1d68fca76ee8c0774f8ae0c06361d47cfefaf
zidanhafiz <nadizsonata@gmail.com>
2024-10-26 03:01:05 +0700

    Fix font subsets

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,9 +1,11 @@
 import { Caveat, Quicksand } from 'next/font/google'
 
 export const caveat = Caveat({
+  subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 })
 
 export const quicksand = Quicksand({
+  subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 })
