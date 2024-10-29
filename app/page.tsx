1a4daee54d595509b7cb6b52b03986dd00a1c136
zidanhafiz <nadizsonata@gmail.com>
2024-10-29 18:19:00 +0700

    Fix card menu layout and add shadow

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h2 className='text-2xl lg:text-3xl font-bold'>Selamat datang di Megumi Sensei App!</h2>
           <p>Megumi Sensei adalah aplikasi yang membantu Anda belajar bahasa Jepang.</p>
         </div>
-        <div className='mt-8 grid grid-cols-2 gap-4'>{menuList.map((item, i) => i > 0 && <CardMenu key={item.name} item={item} />)}</div>
+        <div className='mt-8 grid md:grid-cols-2 gap-4'>{menuList.map((item, i) => i > 0 && <CardMenu key={item.name} item={item} />)}</div>
       </main>
     </div>
   );
